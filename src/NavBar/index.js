fda6ff6de0b7272a8c28b3335aa373fe97c73cef
rufatweb <rufat.web@gmail.com>
2019-07-13 15:31:51 -0400

    change style

--- src/NavBar/index.js
+++ src/NavBar/index.js
@@ -7,7 +7,7 @@ const NavBar = (props) => {
     return (
       <NavContainer>
       <UlTag>
-      <LiTag><LogoImg alt="logo" src="logo_ccbs.JPG" /></LiTag>
+      <LiTag><LogoImg alt="logo" src="logo-ccbs.JPG" /></LiTag>
       <LiTag><Icon name='clock outline' /><H5pContainer><h5>business hours</h5><PTag>Mon-Fri 9am-6pm</PTag></H5pContainer></LiTag>
       <LiTag><Icon name='phone' /><H5pContainer><h5>phone</h5><PTag>(954) 299-5071</PTag></H5pContainer></LiTag>
       <LiTag><Icon name='home' /><H5pContainer><h5>address</h5><PTag>300 E Oakland Park Blvd.</PTag></H5pContainer></LiTag>
@@ -25,6 +25,8 @@ const H5pContainer = styled.div`
 
 const PTag = styled.li`
    height: 10%;
+   font-size: 10px;
+   color: #505050;
 `;
 
 const UlTag = styled.ul`
@@ -41,7 +43,7 @@ const LiTag = styled.li`
 display: flex;
 flex-direction: row;
 text-align: left;
-border-right: 1px solid #888888;
+border-right: 1px solid #A8A8A8;
 
 `;
 

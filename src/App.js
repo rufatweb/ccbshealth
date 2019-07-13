5c8358fb490d6940054f36028c1ea79336879824
Alejandro Mejia <aalejandro.mreyes@icloud.com>
2019-07-13 15:15:21 -0400

    Adds Services

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Banner from "./BannerComponent";
 import NavBar from "./NavBar.js";
+import ServicesContainer from "./ServiceComponent";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Banner />
+      <ServicesContainer />
     </div>
   );
 }
